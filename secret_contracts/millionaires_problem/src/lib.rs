59e53e4189e0a2bdcef632f1570aadd2a6121014
Theo Turner <theo_turner@outlook.com>
2019-11-18 14:19:42 +0000

    Fix contract compilation

--- secret_contracts/millionaires_problem/src/lib.rs
+++ secret_contracts/millionaires_problem/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
-
+extern crate eng_wasm;
+extern crate eng_wasm_derive;
+extern crate serde;
 use eng_wasm::*;
 use eng_wasm_derive::pub_interface;
 use serde::{Serialize, Deserialize};
@@ -21,31 +23,30 @@ struct LocationContract;
 #[pub_interface]
 
 impl LocationContract {
-	// private
-	fn get_locations() -> Vec<Location> {
+        // private
+        fn get_locations() -> Vec<Location> {
         read_state!(LOCATIONS).unwrap_or_default()
     }
 
-	// public
-	pub fn add_location(latitude: i32, longitude: i32) {
-		let mut locations = Self::get_millionaires();
-		locations.push(
-			Location {
-				latitude,
-				longitude,
-			}
-		);
+        // public
+        pub fn add_location(latitude: i32, longitude: i32) {
+                let mut locations = Self::get_locations();
+                locations.push(
+                        Location {
+                                latitude,
+                                longitude,
+                        }
+                );
 
-		write_state!(LOCATIONS => locations);
-	}
+                write_state!(LOCATIONS => locations);
+        }
 
-	pub fn compute_northernmost() -> i32 {
-		match Self::get_locations().iter().max_by_key(|m| m.latitude) {
-			Some(locations) => {
-				location.latitude
-			},
-			None => i32::zero(),
-		}
-	}
+        pub fn compute_northernmost() -> i32 {
+                match Self::get_locations().iter().max_by_key(|m| m.latitude) {
+                        Some(location) => {
+                                location.latitude
+                        },
+                        None => 0 as i32,
+                }
+        }
 }
-
