0dc3711472d31b5b069210c4ab1ac9c55412a39e
Theo Turner <theo_turner@outlook.com>
2019-11-18 17:18:34 +0000

    Core reducer

---
+++ ncl/src/reducers/index.js
@@ -0,0 +1,57 @@
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
+// Responds to initializeEnigma action to save enigma-js client library object
+const initializeEnigmaReducer = (enigma = null, action) => {
+    if (action.type === 'ENIGMA_INITIALIZED') {
+        return action.payload;
+    }
+
+    return enigma;
+};
+
+// Responds to initializeAccounts action to save web3 accounts
+const initializeAccountsReducer = (accounts = [], action) => {
+    if (action.type === 'ACCOUNTS_INITIALIZED') {
+        return action.payload;
+    }
+
+    return accounts;
+};
+
+// Responds to deployLocationContract action to save deployed location secret contract address
+const deployedLocationContractReducer = (deployedLocationContract = null, action) => {
+    if (action.type === 'LOCATION_CONTRACT_DEPLOYED') {
+        return action.payload;
+    }
+
+    return deployedLocationContract;
+};
+
+// Responds to computeNorthernmost action to save northernmost location
+const computeNorthernmostReducer = (northernmostLocation = null, action) => {
+    if (action.type === 'NORTHERNMOST_LOCATION_COMPUTED') {
+        return action.payload;
+    }
+
+    return northernmostLocation;
+};
+
+// Responds to notifyMessage action to save snackbar open status and any contained message
+const notifyMessageReducer = (notification = { open: false, message: '' }, action) => {
+    if (action.type === 'MESSAGE_NOTIFIED') {
+        return action.payload;
+    }
+
+    return notification;
+};
+
+// Combine reducers to state variables named by the keys here; includes a redux-form reducer
+export default combineReducers({
+    enigma: initializeEnigmaReducer,
+    accounts: initializeAccountsReducer,
+    deployedLocationContract: deployedLocationContractReducer,
+    northernmostLocation: computeNorthernmostReducer,
+    notification: notifyMessageReducer,
+    form: formReducer
+});
