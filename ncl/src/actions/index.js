15b5c0e4f8bf990dc75e75378d26e089f3d66aea
Theo Turner <theo_turner@outlook.com>
2019-11-18 17:18:48 +0000

    Web3 and index

---
+++ ncl/src/actions/index.js
@@ -0,0 +1,42 @@
+// Redux action for when enigma-js client library has been initialized
+export const initializeEnigma = (enigma) => {
+    return {
+        type: 'ENIGMA_INITIALIZED',
+        payload: enigma
+    };
+};
+
+// Redux action for when web3 accounts have been initialized
+export const initializeAccounts = (accounts) => {
+    // TODO: change this back to using all accounts after issue is fixed
+    return {
+        type: 'ACCOUNTS_INITIALIZED',
+        //payload: accounts
+        // the 10th account is reserved for the enigma_km - @enigmampc/discovery-cli 0.1.3
+        payload: accounts.slice(0, 9)
+    };
+};
+
+// Redux action for when the contract has been deployed to a particular address
+export const deployLocationContract = (deployedLocationContract) => {
+    return {
+        type: 'LOCATIONS_CONTRACT_DEPLOYED',
+        payload: deployedLocationContract
+    };
+};
+
+// Redux action for when northernmost location has been computed
+export const computeNorthernmost = (northernmostLocation) => {
+    return {
+        type: 'NORTHERNMOST_LOCATION_COMPUTED',
+        payload: northernmostLocation
+    };
+};
+
+// Redux action for notification message has been sent for snackbar display
+export const notifyMessage = (notification) => {
+    return {
+        type: 'MESSAGE_NOTIFIED',
+        payload: notification
+    };
+};
