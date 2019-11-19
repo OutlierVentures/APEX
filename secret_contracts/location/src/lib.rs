11b167561a41c020dbcb78ab477c0d56f41afd1a
Theo Turner <theo_turner@outlook.com>
2019-11-19 15:51:52 +0000

    Code cleanup

--- secret_contracts/location/src/lib.rs
+++ secret_contracts/location/src/lib.rs
@@ -17,36 +17,34 @@ pub struct Location {
     longitude: i32,
 }
 
-
 struct LocationContract;
 
 #[pub_interface]
 
 impl LocationContract {
-        // private
-        fn get_locations() -> Vec<Location> {
+    
+    fn get_locations() -> Vec<Location> {
         read_state!(LOCATIONS).unwrap_or_default()
     }
 
-        // public
-        pub fn add_location(latitude: i32, longitude: i32) {
-                let mut locations = Self::get_locations();
-                locations.push(
-                        Location {
-                                latitude,
-                                longitude,
-                        }
-                );
+    pub fn add_location(latitude: i32, longitude: i32) {
+        let mut locations = Self::get_locations();
+        locations.push(
+            Location {
+                latitude,
+                longitude,
+            }
+        );
 
-                write_state!(LOCATIONS => locations);
-        }
+        write_state!(LOCATIONS => locations);
+    }
 
-        pub fn compute_northernmost() -> i32 {
-                match Self::get_locations().iter().max_by_key(|m| m.latitude) {
-                        Some(location) => {
-                                location.latitude
-                        },
-                        None => 0 as i32,
-                }
+    pub fn compute_northernmost() -> i32 {
+        match Self::get_locations().iter().max_by_key(|m| m.latitude) {
+            Some(location) => {
+                location.latitude
+            },
+            None => 0 as i32,
         }
+    }
 }
