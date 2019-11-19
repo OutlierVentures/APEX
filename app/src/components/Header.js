bf8fe83ba14b10aaefaab71c56d4eeccfc5a0d69
Theo Turner <theo_turner@outlook.com>
2019-11-19 17:37:09 +0000

    Inline react styles

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -23,9 +23,9 @@ class Header extends Component {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
-                <AppBar position="static">
+                <AppBar position="static" style={{backgroundColor: "#222222"}}>
                     <Toolbar>
-                        <img id="ovlogo" src="https://outlierventures.io/wp-content/themes/ov/assets/images/outlierventureslogo.svg" />
+                        <img style={{height: "1em"}} src="https://outlierventures.io/wp-content/themes/ov/assets/images/outlierventureslogo.svg" />
                         <Typography
                             variant="subtitle1"
                             color="inherit"
