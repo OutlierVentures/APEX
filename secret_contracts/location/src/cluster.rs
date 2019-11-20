37c912cc67dacbaf1e30afe247b997e1a4bb8ac6
Theo Turner <theo_turner@outlook.com>
2019-11-20 12:58:38 +0000

    Add a clustering contract

---
+++ secret_contracts/location/src/cluster.rs
@@ -0,0 +1,60 @@
+#![no_std]
+extern crate eng_wasm;
+extern crate eng_wasm_derive;
+extern crate serde;
+extern crate cogset;
+use eng_wasm::*;
+use eng_wasm_derive::pub_interface;
+use serde::{Serialize, Deserialize};
+use cogset::{Euclid, Kmeans};
+
+// Encrypted state keys
+static LOCATIONS: &str = "locations";
+
+// Structs
+#[derive(Serialize, Deserialize)]
+pub struct Location {
+    // Multiply by 1M - contracts only support integers
+    latitude: i32,
+    longitude: i32,
+}
+
+struct LocationContract;
+
+#[pub_interface]
+
+impl LocationContract {
+
+    fn get_locations() -> Vec<Location> {
+        read_state!(LOCATIONS).unwrap_or_default()
+    }
+
+    pub fn add_location(latitude: i32, longitude: i32) {
+        let mut locations = Self::get_locations();
+        locations.push(Location{latitude, longitude});
+        write_state!(LOCATIONS => locations);
+    }
+
+    pub fn cluster() -> (Vec<i32>, Vec<i32>) {
+        let locations = Self::get_locations();
+        let mut eucvec: Vec<Euclid<_>> = Vec::new();
+        for point in &locations {
+            eucvec.push(Euclid([point.latitude as f64, point.longitude as f64]));
+        }
+        let k = 3;
+        let kmeans = Kmeans::new(&eucvec, k);
+        let clusters = kmeans.clusters();
+        let mut xvec: Vec<i32> = Vec::new();
+        let mut yvec: Vec<i32> = Vec::new();
+        for result in &clusters {
+            xvec.push((result.0).0[0] as i32);
+            yvec.push((result.0).0[0] as i32);
+        }
+        (xvec, yvec)
+    }
+}
+
+
+
+
+
