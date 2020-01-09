8fd9d786a09f131a9b9967da686601f138be0588
Theo Turner <theo_turner@outlook.com>
2020-01-09 11:02:39 +0000

    React: add training data for classification

--- app/src/components/LocationContract.js
+++ app/src/components/LocationContract.js
@@ -172,6 +172,68 @@ class LocationContract extends Component {
         }
     }
 
+    // Redux form callback when add training data is submitted
+    async onAddLocationWithClass({ locationwithclassstring } ) {
+        // Sanitise input - check for valid JSON of latitudes / longitudes.
+        try {
+            var locations = JSON.parse(locationwithclassstring);
+            assert(Array.isArray(locations));
+            for (var i = 0; i < locations.length; i++) {
+                var keys = Object.keys(locations[i]);
+                assert(keys.length === 3);
+                assert(keys[0] === "latitude");
+                assert(keys[1] === "longitude");
+                assert(keys[2] === "class");
+                assert(typeof locations[i].latitude === "number" && Math.abs(locations[i].latitude) < 90.0);
+                assert(typeof locations[i].longitude === "number" && Math.abs(locations[i].longitude) < 180.0);
+                assert(typeof locations[i].class === "number" && Math.abs(locations[i].class) >= 0.0);
+            }
+        } catch {
+            openSnackbar({ message: 'Invalid input: must be array of JSON latitude/longitude/class points' });
+            return
+        }
+        // Create compute task metadata
+        // computeTask(
+        //      fn - the signature of the function we are calling (Solidity-types, no spaces)
+        //      args - the args passed into our method w/ format [[arg_1, type_1], [arg_2, type_2], …, [arg_n, type_n]]
+        //      gasLimit - ENG gas units to be used for the computation task
+        //      gasPx - ENG gas price to be used for the computation task in grains format (10⁸)
+        //      sender - Ethereum address deploying the contract
+        //      scAddr - the secret contract address for which this computation task belongs to
+        // )
+        const taskFn = 'add_training_data(string)';
+        // Multiply by 1M as contracts only take ints
+        const taskArgs = [[locationwithclassstring, 'string']];
+        const taskGasLimit = 10000000;
+        const taskGasPx = utils.toGrains(1e-7);
+        let task = await new Promise((resolve, reject) => {
+            this.props.enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, this.props.accounts[0],
+                this.props.deployedLocationContract)
+                .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+                .on(eeConstants.ERROR, (error) => {
+                    if (error.hasOwnProperty('message')){
+                        openSnackbar({ message: error.message});
+                    } else {
+                        openSnackbar({ message: 'Failed to add training data'});
+                    }
+                    reject(error);
+                });
+        });
+        openSnackbar({ message: 'Task pending: adding training data' });
+        while (task.ethStatus === 1) {
+            // Poll for task record status and finality on Ethereum after worker has finished computation
+            task = await this.props.enigma.getTaskRecordStatus(task);
+            await sleep(1000);
+        }
+        // ethStatus === 2 means task has successfully been computed and committed on Ethereum
+        task.ethStatus === 2 ?
+            openSnackbar({ message: 'Task succeeded: added training data' })
+            :
+            openSnackbar({ message: 'Task failed: did not add training data' })
+        ;
+        this.props.reset('addLocationWithClass');
+    }
+
     render() {
         if (this.props.deployedLocationContract === null) {
             return (
