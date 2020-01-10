72c768dd826a050768fbf169caf160d739ad04d8
Theo Turner <theo_turner@outlook.com>
2020-01-10 12:48:56 +0000

    Interop improvements for Store.sol

--- smart_contracts/Store.sol
+++ smart_contracts/Store.sol
@@ -1,21 +1,21 @@
 
-pragma solidity ^0.5.13;
+pragma solidity ^0.5.0;
 contract Chainterface {
 
     string serialisedData;
     address owner;
-    
+
     constructor() public {
     }
 
     function writeData(string memory data) public {
         if (msg.sender == owner) {
-            serialisedData = data; 
+            serialisedData = data;
         }
     }
-    
+
     function readData() public view returns (string memory) {
         return serialisedData;
-    } 
+    }
 
 }
